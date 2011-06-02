e386f86fce76e34bdd55ea6e84e8588dfedf00f9
Arto Bendiken <arto@bendiken.net>
2011-06-02 19:04:54 +0200

    Rebooted the project development.

--- lib/rdfize.rb
+++ lib/rdfize.rb
@@ -1,2 +1,6 @@
-require 'rdf'
-require 'rdfize/version'
+require 'rdf'        # @see http://rubygems.org/gems/rdf
+require 'rdf/raptor' # @see http://rubygems.org/gems/rdf-raptor
+
+module RDFize
+  autoload :VERSION, 'rdfize/version'
+end
--- lib/rdfize/extractors.rb
+++
@@ -1,4 +0,0 @@
-require 'rdfize/extractors/exif/exifr'
-require 'rdfize/extractors/id3/id3lib'
-require 'rdfize/extractors/pdf/pdf-toolkit'
-require 'rdfize/extractors/vcard/vpim'
