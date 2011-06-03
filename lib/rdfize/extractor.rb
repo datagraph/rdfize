c4c7c84a96212d15c3d709cc39c052ecc40d00ef
Arto Bendiken <arto@bendiken.net>
2011-06-03 14:19:01 +0200

    Implemented the Extractor.each method.

--- lib/rdfize/extractor.rb
+++ lib/rdfize/extractor.rb
@@ -1,5 +1,22 @@
 module RDFize
   class Extractor
+    extend Enumerable
+
+    ##
+    # @yield  [klass]
+    # @yieldparam   [Class] klass
+    # @yieldereturn [void] ignored
+    # @return [Enumerator]
+    def self.each(&block)
+      if block_given?
+        @extractors ||= {}
+        @extractors.each_value do |values|
+          values.each(&block)
+        end
+      end
+      enum_for(:each)
+    end
+
     ##
     # @param  [Hash{Symbol => Object}] options
     def initialize(options = {})
